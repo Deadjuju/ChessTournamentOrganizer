db065e106648a3c475c91cdff3406561b30c2f89
Julien Garcia <julien.garcia-lp@laposte.net>
2022-09-05 23:25:02 +0200

    (settings): add db_config file

---
+++ Settings/db_config.py
@@ -0,0 +1,10 @@
+"""Database project configuration"""
+
+from tinydb import TinyDB
+from pathlib import Path
+
+ROOTS = Path(__file__).resolve().parent.parent
+
+# Save in DataBase at Project roots
+DB = TinyDB(ROOTS / 'db.json', indent=4)
+PLAYERS_TABLE = DB.table("Players")
