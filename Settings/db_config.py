cd211f14bd29ba01919b2cede69e5d53a5fb595d
Julien Garcia <julien.garcia-lp@laposte.net>
2022-09-13 22:49:36 +0200

    (settings): create tournament table

--- Settings/db_config.py
+++ Settings/db_config.py
@@ -8,3 +8,4 @@ ROOTS = Path(__file__).resolve().parent.parent
 # Save in DataBase at Project roots
 DB = TinyDB(ROOTS / 'db.json', indent=4)
 PLAYERS_TABLE = DB.table("Players")
+TOURNAMENTS_TABLE = DB.table("Tournaments")
