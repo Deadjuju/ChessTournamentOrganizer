a22f7ab994a65edaa7ec7facd04a39152ec18dca
Julien Garcia <julien.garcia-lp@laposte.net>
2022-09-13 19:22:51 +0200

    (utils): fix error with no answer

--- Utils/exceptions.py
+++ Utils/exceptions.py
@@ -2,10 +2,8 @@
 
 
 class NotValidChoiceError(Exception):
-    def __init__(self, message):
-        super().__init__(message)
+    ...
 
 
 class EmptyFieldError(Exception):
-    def __init__(self, message):
-        super().__init__(message)
+    ...
--- Utils/validators.py
+++ Utils/validators.py
@@ -3,6 +3,9 @@ from Utils.exceptions import NotValidChoiceError, EmptyFieldError
 
 
 def check_multiple_choice(user_choice, choices):
+    if not user_choice:
+        message = f"Invalid user choice: {user_choice}."
+        raise NotValidChoiceError(message)
     for choice in choices:
         if user_choice in choice[0]:
             return choices[int(user_choice) - 1][-1]
